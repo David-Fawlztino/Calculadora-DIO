09da39c05830ed1940026de829a53de3ddf6a4a6
David_Fawlztino <dfawlstino@gmail.com>
2026-07-09 06:40:56 -0300

    Projeto Pronto

---
+++ src/components/Button/styles.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export const ButtonContainer = styled.button`
+    padding: 30px;
+    border: 1px solid #9b9a9a;
+    background-color: #1a1919;
+    color: #dddddd;
+    
+    font-size: 20px;
+    font-weight: bold;
+    
+    flex: 1;
+
+    &:hover {
+        opacity: 0.9;
+    }
+
+`
---
+++ src/components/Input/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #1a1919;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    
+    
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #1a1919;
+        border: 2px solid #9b9a9a;
+        float: right;
+        clear: both;
+        padding: 0 10px;
+
+        font-size: 24px;
+        font-family: 'Roboto', sans-serif;
+        color: #dddddd;
+    }
+
+`
